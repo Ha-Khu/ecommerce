af794dd0b47d6329cc9dfcd5cad398b4feab648f
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-16 18:04:31 +0200

    feat: server setup with db connection

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, ()=>{
+  console.log(`Example app listening on port ${port}`)
+})

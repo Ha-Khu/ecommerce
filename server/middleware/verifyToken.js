884810e3c0a335ce91c11fabe9ff8b9024a25ea4
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-18 17:26:15 +0200

    feat: add middleware for token authentication

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(req, res, next){
+  const token = req.headers.authorization?.split(' ')[1]
+  if(!token){
+    res.status(401).json({error: "Neplatný token"})
+    return
+  }
+  try{
+    const decode = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decode
+    next()
+  } catch(err){
+    res.status(401).json({error: "Neplatný token"})
+  }
+}
+
+
+module.exports = verifyToken

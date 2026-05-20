66ddeda3e9658f0b9b14cce49275d7ec756f2107
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-20 19:00:16 +0200

    feat: add GET single, PUT and DELETE product routes for admin

--- server/routes/products.js
+++ server/routes/products.js
@@ -15,6 +15,17 @@ router.get("/", async (req, res)=>{
   }
 })
 
+router.get("/:id", async (req, res)=>{
+  try{
+    let sql = "SELECT * FROM products WHERE id = ?"
+    const id = req.params.id
+    const [rows] = await db.query(sql, [id])
+    res.json(rows)
+  } catch(err){
+    res.status(500).json({err: "Loading failed"})
+  }
+})
+
 router.post("/", verifyToken, isAdmin, async (req, res)=>{
   try{
     const {name, price, quantity, description, category_id} = req.body
@@ -23,5 +34,28 @@ router.post("/", verifyToken, isAdmin, async (req, res)=>{
     res.status(201).json({message: "Product inserted"})
   } catch(err){
     res.status(500).json({err: "Insert failed"})
+  }
+})
+
+router.delete("/:id", verifyToken, isAdmin, async (req, res)=>{
+  try {
+  const id = req.params.id
+  let sql = "DELETE FROM products WHERE id = ?"
+  const [rows] = await db.query(sql, [id])
+  res.status(200).json({message: "Delete successful"})
+  } catch(err){
+    res.status(500).json({err: "Delete failed"})
+  }
+})
+
+router.put("/:id", verifyToken, isAdmin, async (req, res)=>{
+  try {
+  const id = req.params.id
+  const {name, price, quantity, description, category_id} = req.body
+  let sql = "UPDATE products SET name = ?, price = ?, quantity = ?, description = ?, category_id = ? WHERE id = ?"
+  const [rows] = await db.query(sql, [name, price, quantity, description, category_id, id])
+  res.status(200).json({message: "Update successful"})
+  } catch(err){
+    res.status(500).json({err: "Update failed"})
   }
 })

c179e4d2d2956b141fd0c6ab4694e8a168d2b649
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-23 20:33:25 +0200

    feat: add cart routes (GET, POST, DELETE)

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const app = express()
 const port = process.env.PORT
 const authRouter = require('./routes/auth')
 const productRouter = require('./routes/products')
+const cartRouter = require('./routes/cart')
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.listen(port, ()=>{
   console.log(`Example app listening on port ${port}`)
---
+++ server/routes/cart.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const verifyToken = require('../middleware/verifyToken')
+
+router.get("/", verifyToken, async (req, res)=>{
+  try{
+    const id = req.user.id
+    let sql = "SELECT * FROM cart WHERE user_id = ?"
+    const [rows] = await db.query(sql, [id])
+    res.json(rows)
+  }catch(err){
+    res.status(500).json({err: "Loading Failed"})
+  }
+})
+
+router.post("/", verifyToken, async (req, res)=>{
+  try {
+    const {quantity, product_id} = req.body
+    const user_id = req.user.id
+    let sql = "INSERT INTO cart(quantity, user_id, product_id) VALUES (?, ?, ?)"
+    const [rows] = await db.query(sql, [quantity, user_id, product_id])
+    res.status(201).json({message: "Cart Inserted"})
+  }catch(err){
+    res.status(500).json({err: "Insert Failed"})
+  }
+})
+
+router.delete("/:id", verifyToken, async (req, res)=>{
+  try{
+    const id = req.params.id
+    let sql = "DELETE FROM cart WHERE id = ?"
+    const [rows] = await db.query(sql, [id])
+    res.status(200).json({message: "Cart Deleted"})
+  }catch(err){
+    res.status(500).json({err: "Delete Failed"})
+  }
+})
+
+module.exports = router

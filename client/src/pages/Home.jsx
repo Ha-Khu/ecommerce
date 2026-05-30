5b0c4208d0de0e9594af85dcda172b2ea75dbc7a
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-30 22:13:38 +0200

    feat: add Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,51 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import { useEffect } from "react"
+import {useState} from 'react'
+import axios from 'axios'
+
 function Home(){
-  return <div>Home</div>
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
+
+  useEffect(()=>{
+    async function fetchProducts(){
+      try{
+      const response = await axios.get("http://localhost:5000/api/products")
+      setProducts(response.data)
+      setLoading(false)
+      }catch(err){
+        setError("Laoding of products failed, try again")
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  if(loading) return(
+    <p>Loading...</p>
+  )
+
+  return(
+    <Card>
+      <CardHeader>
+        <CardTitle>Home</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {
+          products.map((products)=>(
+            <div key={products.id}>
+              <p>{products.name}</p>
+              <p>{products.price}</p>
+              <p>{products.quantity}</p>
+              <p>{products.description}</p>
+            </div>
+          ))
+        }
+      </CardContent>
+    </Card>
+  )
 }
 
 export default Home

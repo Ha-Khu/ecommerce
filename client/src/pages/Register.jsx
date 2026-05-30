848cc2770f0a52e89f46652e055b3dbfb0e3a3e0
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-30 21:49:24 +0200

    feat: add register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,62 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import {useState} from 'react'
+import axios from 'axios'
+import { useNavigate, Link } from "react-router-dom"
+
 function Register(){
-  return <div>Register</div>
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  async function handleRegister(){
+    try{
+      await axios.post("http://localhost:5000/api/auth/register", {firstName, lastName, email, password})
+      navigate("/login")
+    }catch(err){
+      setError("Registration Failed, please try again")
+    }
+  }
+
+  return(
+    <Card>
+      <CardHeader>
+        <CardTitle>Register</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Input
+          type="text"
+          placeholder="Jacob"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Vance"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Input
+          type="email"
+          placeholder="jacob@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="*******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p>{error}</p>}
+        <Button onClick={handleRegister}>Register</Button>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default Register
